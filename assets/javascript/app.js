ba7b5eaf83be303ca26260eb713d699a8546b514
KB Total Recall <kanwar@gmail.com>
2019-03-04 23:39:55 -0800

    Rewrite part of the code to align images. Broke freeze unfreeze functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,7 +7,7 @@ var addAnimal;
 function showAnimal(animalName){
 
    // event.preventDefault();
-    $("#animals-div").empty();
+    
 
     AddURL = "https://api.giphy.com/v1/gifs/search?api_key=zn11jQQ2k0hkF7FUnGA39VN9cZRbr5d6&q=" + animalName + "&limit=25&offset=0&rating=G&lang=en" ;
 
@@ -16,21 +16,29 @@ function showAnimal(animalName){
         url: AddURL
     }).then(function (response){
         console.log(response.data);
-        $("#search-animal").append("<div id='animals-div'></div>") ;
+       
+       // $("#search-animal").append("<div id='animals-div'></div>") ;
         $(response.data).each(function (index, items) {
-            $("#animals-div").append("<img  id=" + "'" +items.title + "'" + " class='img-click img-thumbnail p-2' src=" + items.images.fixed_width_small_still.url + " data-status='still'" + " data-animate=" + items.images.fixed_width_small.url + " data-still=" + items.images.fixed_width_small_still.url + ">");
-            
+            var cardDiv = $("<div>");
+            var cardDivId = 'animals-div'+index ;
+            cardDiv.addClass("animalclass card p-4");
+            cardDiv.attr("id" , cardDivId);
+            // Append one image to per div
+            $(cardDiv).append("<img  id=" + index + " class='img-click  img-thumbnail  p-2' src=" + items.images.fixed_width_small_still.url + " data-status='still'" + " data-animate=" + items.images.fixed_width_small.url + " data-still=" + items.images.fixed_width_small_still.url + ">");
+           // append the Div to the Parent Div
+            $("#search-animal").append(cardDiv);
         });
 
-        $(".img-click").on("click", function(){
-            //event.preventDefault();
-            console.log($(this).attr('data-status'));
+        $("img").click(function(){
+            event.preventDefault();
+            
             imageStatus = $(this).attr('data-status');
-            if(imageStatus =='still'){
+           console.log(imageStatus, $(this).attr('id'));
+            if(imageStatus ==='still'){
                 $(this).attr("src", $(this).attr("data-animate"));
                 $(this).attr('data-status','animate');
             }
-            else{
+            else {
                 $(this).attr("src", $(this).attr("data-still"));
                 $(this).attr('data-status','still');
             }
@@ -39,19 +47,23 @@ function showAnimal(animalName){
     })
 }
 
-
 function renderButtons() {
+    var buttonDiv = $("<div>");
+    buttonDiv.addClass("anim-card anim-button");
+    buttonDiv.attr("id" , "animalButtonDiv");
     for(i=0;i< animalArr.length; i++){
-       $("#search-animal").append("<button class='anim-button bg-primary m-2' id=" + animalArr[i] + " value=" + animalArr[i] + ">" + animalArr[i] + "</button>");
+       $(buttonDiv).append("<button class='anim-button bg-warning  m-2' id=" + animalArr[i] + " value=" + animalArr[i] + ">" + animalArr[i] + "</button>");
     }
+    $("#search-animal").append(buttonDiv);
 }
 
 renderButtons();
 
 $(".anim-button").on("click", function(event){
-    event.preventDefault();
+    //event.preventDefault();
     var animalName = $(this).val();
 
+    $('#search-animal').find('*').not('.anim-button').remove();
     showAnimal(animalName);
 
 });
